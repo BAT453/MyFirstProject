c6c70b6a340f788a682c9abd104153891f99ac3e
BAT453 <34679127+BAT453@users.noreply.github.com>
2018-03-17 10:42:43 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4; 
+            int s = 0; 
+            for (int k = 1; k-- >= n ; s += k) 
+                Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
